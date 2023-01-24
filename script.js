6926bfd7dd99acb5f4867495de04aa0916656f43
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-01-24 17:29:14 +0100

    fix scritp.js by adding working functions

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-const inputValue = document.querySelector(".js-inputValue")
-const outputValue = document.querySelector(".js-outputValue")
-const form = document.querySelector(".exchanger")
-
-const checkExchangeRatio = (inCurrency, outCurrency) => {
-    if(inCurrency == outCurrency){
-        exchangeRatio = 1
-    } else {
-    exchangeDirection = `${inCurrency}->${outCurrency}`
-    switch(exchangeDirection) {
-        case 'EUR->PLN':
-            exchangeRatio = 4.7186
-            break;
-        case 'PLN->EUR':
-            exchangeRatio = 1 / 4.7186
-            break;
+{
+    const checkExchangeRatio = (inCurrency, outCurrency) => {
+        if(inCurrency == outCurrency){
+            exchangeRatio = 1
+        } else {
+            exchangeDirection = `${inCurrency}->${outCurrency}`
+            switch(exchangeDirection) {
+                case 'EUR->PLN':
+                    exchangeRatio = 4.7186
+                break;
+                case 'PLN->EUR':
+                    exchangeRatio = 1 / 4.7186
+                break;
+            }
+        }       
     }
+    const exchangeCurrency = (inValue) => {
+            const inCurrency = document.querySelector(".js-inputCurrency").value
+            const outCurrency = document.querySelector(".js-outputCurrency").value
+            checkExchangeRatio(inCurrency, outCurrency)
+            const outValue = inValue * exchangeRatio
+            const outputValue = document.querySelector(".js-outputValue")
+            outputValue.value = outValue.toFixed(2)
     }
-}
+    const onFormSubmit = e => {
+        e.preventDefault();
+        const inValue = document.querySelector(".js-inputValue").value
+        exchangeCurrency(inValue);
+    }
+    const init = () => {
+        const form = document.querySelector(".exchanger")
+        form.addEventListener('submit', onFormSubmit);
+    }
 
-const exchangeCurrency = (inValue) => {
-    inCurrency = document.querySelector(".js-inputCurrency").value
-    outCurrency = document.querySelector(".js-outputCurrency").value
-    checkExchangeRatio(inCurrency, outCurrency)
-    const outValue = inValue * exchangeRatio 
-    outputValue.value = outValue.toFixed(2)
+    init()
 }
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    const inValue = inputValue.value
-    exchangeCurrency(inValue);
-});
